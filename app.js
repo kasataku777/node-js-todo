7b56317479dfe0d1db300f8db3d52da11b16c2ce
kasataku777 <takumi.jihen@gmail.com>
2022-09-23 18:04:04 +0900

    deploy

---
+++ app.js
@@ -0,0 +1,24 @@
+const express  = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+app.use(express.json());
+app.use(express.static("./public"));
+const PORT = 5001;
+
+app.use("/api/v1/tasks",taskRoute);
+
+//DBと接続
+const start = async ()=>{
+	try{
+		await connectDB(process.env.MONGO_URL);
+		app.listen(PORT,console.log("サーバーが起動しました"));
+	}catch(err){
+		console.log(err);
+	}
+}
+
+start();
+
+
